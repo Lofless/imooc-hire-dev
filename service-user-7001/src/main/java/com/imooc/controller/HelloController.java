7bbef957895c061469f1e7cc0230c642d1b78dac
yangguang <3141219704@qq.com>
2025-08-14 15:15:53 +0800

    测试user的api接口

--- service-user-7001/src/main/java/com/imooc/controller/HelloController.java
+++ service-user-7001/src/main/java/com/imooc/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.Stu;
+import com.imooc.service.StuService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("u")
 public class HelloController {
 
+    private final StuService stuService;
+
+    public HelloController(StuService stuService) {
+        this.stuService = stuService;
+    }
+
     @GetMapping("hello")
     public Object hello(){
         return "Hello UserService~~";
+    }
+
+    @GetMapping("stu")
+    public Object stu(){
+        Stu stu = new Stu();
+        stu.setAge(18);
+        stu.setName("imooc");
+        stu.setId(1010);
+
+        stuService.save(stu);
+        return "ok";
     }
 }
