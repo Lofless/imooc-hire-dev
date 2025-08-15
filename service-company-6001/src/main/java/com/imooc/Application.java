04489734edbffbb3971e899429707a8d7e6c3ca0
yangguang <3141219704@qq.com>
2025-08-15 20:18:05 +0800

    将服务实例注册到Nacos中：添加依赖，修改yml配置文件，添加注解在启动类中

--- service-company-6001/src/main/java/com/imooc/Application.java
+++ service-company-6001/src/main/java/com/imooc/Application.java
@@ -2,8 +2,10 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // 开启注册中心的服务注册和发现功能
 public class Application {
 
     public static void main(String[] args) {
--- service-user-7001/src/main/java/com/imooc/Application.java
+++ service-user-7001/src/main/java/com/imooc/Application.java
@@ -4,9 +4,11 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.imooc.mapper")
+@EnableDiscoveryClient // 开启注册中心的服务注册和发现功能
 public class Application {
 
     public static void main(String[] args) {
