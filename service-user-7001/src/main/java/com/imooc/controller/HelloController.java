1cd9b64c1bce25c0bf19101918cafa377fe0bf09
yangguang <3141219704@qq.com>
2025-08-16 16:35:25 +0800

    增加gateway的配置文件，并使用负载均衡创建网关来代替user-service和company-service的端口

--- service-user-7001/src/main/java/com/imooc/controller/HelloController.java
+++ service-user-7001/src/main/java/com/imooc/controller/HelloController.java
@@ -2,12 +2,15 @@ package com.imooc.controller;
 
 import com.imooc.pojo.Stu;
 import com.imooc.service.StuService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("u")
+@Slf4j
 public class HelloController {
 
     private final StuService stuService;
@@ -16,8 +19,12 @@ public class HelloController {
         this.stuService = stuService;
     }
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("hello")
     public Object hello(){
+        log.info("port:{}",port);
         return "Hello UserService~~";
     }
 
